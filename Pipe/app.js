93d79cf05645f88cddc9e286b96b48983fdf00e3
unknown <prashantpattanashetti5@gmail.com>
2025-06-23 13:41:14 +0530

    passing JSON to server

--- Pipe/app.js
+++ Pipe/app.js
@@ -3,9 +3,13 @@ const fs =require('fs');
 
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/html'});
-    const readStream = fs.createReadStream(__dirname +'/index.html','utf-8');
-    readStream.pipe(res);
+    res.writeHead(200,{'content-type':'application/json'});
+    const person = {
+        name: 'alex',
+        email:'alex@gmail.com',
+        job:'developer'
+    };
+    res.end(JSON.stringify(person));
 });
 
 server.listen(3000,'127.0.0.1'); 
